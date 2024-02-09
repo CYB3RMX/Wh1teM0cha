fe7b72d6d41c3b3027f352c4625438b4b296596b
CYB3RMX <cyb3rmx0@gmail.com>
2024-02-09 18:20:08 +0300

    News 09/02/2024 18:20 -> Hotfix.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 license",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
